2654bb9e4f2a0cd010d6e3bff6444f1e02dead93
Anthony Lapenok <anton.lapenok@gmail.com>
2025-02-13 15:20:43 +0300

    initial commit

---
+++ src/main/java/org/example/TestTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class TestTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskApplication.class, args);
+    }
+}
