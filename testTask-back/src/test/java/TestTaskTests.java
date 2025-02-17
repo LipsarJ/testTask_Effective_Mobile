c15d0bd6c2db85a7944c0b3e807976dd418b56de
Anthony Lapenok <anton.lapenok@gmail.com>
2025-02-17 19:36:12 +0300

    Update TestTaskTests.java

--- testTask-back/src/test/java/TestTaskTests.java
+++ testTask-back/src/test/java/TestTaskTests.java
@@ -1,9 +1,10 @@
+import org.example.TestTaskApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(classes = TestTaskApplication.class)
 @ActiveProfiles("test")
 @Testcontainers
 class TestTaskTests {
